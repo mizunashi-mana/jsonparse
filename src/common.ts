dbb35dbeca7b5319e754c97b8a50fb7b4dd1c12f
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2015-10-18 12:18:55 +0900

    add comments on common

--- src/common.ts
+++ src/common.ts
@@ -9,12 +9,38 @@ import {
   mapSuccess,
 } from "./parseresult/result";
 
+/**
+ * base function type of {@link Parser}
+ *
+ * @param obj source success object
+ * @returns result parsed source object (success or failure)
+ */
 export type ParseFunc<T, U> = (obj: SuccessObjType<T>) => ParseResult<U>;
 
+/**
+ * make Success Object from source success object and converted value
+ *
+ * @param obj source success object
+ * @param convObj converted success value
+ * @returns new success object including converted value
+ * @param T source object type
+ * @param U converted object type
+ */
 export function makeSuccess<T, U>(obj: SuccessObjType<T>, convObj: U) {
   return ParseResult.success(mapSuccess((v) => convObj, obj));
 }
 
+/**
+ * make Success Object from source success object and converted value
+ *
+ * @param obj source success object
+ * @param msg failure message
+ * @param exp expected type
+ * @param childs error childs nodes
+ * @returns new success object including converted value
+ * @param T source object type
+ * @param U converted object type
+ */
 export function makeFailure<T, U>(obj: SuccessObjType<T>, msg?: string, exp?: string, childs?: ParseErrorNode[]) {
   return ParseResult.fail<U>({
     value: new ParseErrorStocker(msg, exp, JSON.stringify(obj.value), childs),
@@ -22,7 +48,22 @@ export function makeFailure<T, U>(obj: SuccessObjType<T>, msg?: string, exp?: st
   });
 }
 
+/**
+ * alias function type of {@link ParseFunc} for {@link mapParseResult}
+ *
+ * @param obj source success value
+ * @returns result parsed source value
+ */
 export type MapperParseResult<T, U> = (obj: T) => ParseResult<U>;
+
+/**
+ * wrapper and builder for {@link ParseFunc}
+ *
+ * @param f receive makeSuccess and makeFailure and create parse function for {@link Parser}
+ * @returns parse function for {@link Parser}
+ * @param T source object type
+ * @param U converted object type
+ */
 export function mapParseResult<T, U>(f: (
   mkS: (convObj: U) => ParseResult<U>,
   mkF: (msg: string, exp?: string) => ParseResult<U>
@@ -33,11 +74,31 @@ export function mapParseResult<T, U>(f: (
   )(obj.value);
 }
 
+/**
+ * Parser class including parse function ({@link ParseFunc})
+ *
+ * @param T source object type
+ * @param U converted object type
+ */
 export class Parser<T, U> {
+  /**
+   * having parse function
+   */
   private action: ParseFunc<T, U>;
+
+  /**
+   * @param fn inner parse function
+   */
   constructor(fn: ParseFunc<T, U>) {
     this.action = fn;
   }
+
+  /**
+   * parse source object with this parser
+   *
+   * @param obj source object
+   * @returns result of parsing
+   */
   parse(obj: SuccessObjType<T>): ParseResult<U> {
     return this.action(obj);
   }
