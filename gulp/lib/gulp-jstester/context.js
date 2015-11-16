a428f32e2474ed77e6c57fd24e442cad60ba244b
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2015-11-16 10:23:25 +0900

    fix gulp bases and pkg infos
    Warning:
    Most of gulp tasks is not running!  I will fix problems as soon.

--- gulp/lib/gulp-jstester/context.js
+++ gulp/lib/gulp-jstester/context.js
@@ -6,16 +6,39 @@ var fs = require("fs");
 var mkdirp = require("mkdirp").sync;
 
 module.exports.runJsFile = function(filename, content) {
+  var result = undefined;
+  var is_error = false;
   var tmpfilename = path.join(__dirname, "tmp", path.relative(process.cwd(), filename));
   var contenttmp = '___dirname = __dirname;__dirname = "' + path.dirname(filename) + '";';
-  mkdirp(path.dirname(tmpfilename));
-  if (content instanceof Buffer) {
-    contenttmp = new Buffer(contenttmp);
-    contenttmp = Buffer.concat([contenttmp, content], contenttmp.length + content.length);
-    fs.writeFileSync(tmpfilename, contenttmp, 0, contenttmp.length);
-  } else {
-    contenttmp = contenttmp + content;
-    fs.writeFileSync(tmpfilename, contenttmp, 0, "utf-8");
+
+  try {
+    mkdirp(path.dirname(tmpfilename));
+
+    if (content instanceof Buffer) {
+      contenttmp = new Buffer(contenttmp);
+      contenttmp = Buffer.concat([contenttmp, content], contenttmp.length + content.length);
+      fs.writeFileSync(tmpfilename, contenttmp, 0, contenttmp.length);
+    } else {
+      contenttmp = contenttmp + content;
+      fs.writeFileSync(tmpfilename, contenttmp, 0, "utf-8");
+    }
+  } catch(e) {
+    throw new Error("Failed to run js file.");
   }
-  return require(tmpfilename);
+
+  // running
+  try {
+    result = require(tmpfilename);
+  } catch(e) {
+    is_error = true;
+    result = e;
+  }
+
+  fs.unlink(tmpfilename, function(err) {});
+
+  if (is_error) {
+    throw result;
+  } else {
+    return result;
+  }
 };
