7c2d0184f4e5f640927883962e0989a4d1584ff2
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2015-11-26 22:59:00 +0900

    add documents of hash parser

---
+++ docs/examples/parsers/hash.js
@@ -0,0 +1,31 @@
+var sparse = require("sonparser");
+var assert = require("assert");
+
+assert.deepEqual(
+  sparse.hash(sparse.boolean).parse({}),
+  {}
+); // success
+
+assert.deepEqual(
+  sparse.hash(sparse.boolean).parse({"a": true, "b": true}),
+  {"a": true, "b": true}
+); // success
+
+assert.deepEqual(
+  sparse.hash(sparse.boolean).parse({"0": true, "1": true}),
+  {"0": true, "1": true}
+); // success
+
+assert.throws(
+  function() {
+    return sparse.hash(sparse.boolean).parse("not hash object");
+  },
+  sparse.ConfigParseError
+); // failure
+
+assert.throws(
+  function() {
+    return sparse.hash(sparse.boolean).parse({"a": "not boolean"});
+  },
+  sparse.ConfigParseError
+); // failure
